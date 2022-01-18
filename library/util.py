7a4efb95636a28134ee411da0cbf9339513fb3f3
张骏毅 <zhangjunyi@zhiyitech.cn>
2022-01-18 18:08:46 +0800

    车票展示

--- library/util.py
+++ library/util.py
@@ -1,4 +1,5 @@
 import os
+import prettytable as pt
 
 proxy_host = 'http-dyn.abuyun.com'
 proxy_port = '9020'
@@ -8,3 +9,59 @@ proxies = {
     'http': f'http://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}',
     'https': f'http://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}'
 }
+
+
+def log(s, color=None):
+    s = str(s)
+    if color == 'r':
+        return "\033[31m%s\033[0m" % s
+    elif color == 'y':
+        return "\033[33m%ds\033[0m" % s
+    elif color == 'g':
+        return "\033[32m%s\033[0m" % s
+    elif color == 'b':
+        return "\033[36m%s\033[0m" % s
+    else:
+        return s
+
+
+class ticket_sheet:
+    def __init__(self, fields: list = None):
+        self.pt = pt.PrettyTable()
+        self.fields = fields or ['车次', '车起始', '车终点', '起始站', '终点站', '出发时间', '到达时间', '历时', '当日到达', '商务座', '一等座',
+                                 '二等座', '高级软卧', '一等卧', '动卧', '二等卧', '软卧', '硬座', '无座']
+        self.pt.field_names = self.fields
+        self.pt.align = 'l'
+
+    def add_train(self, train):
+        train_fields = list()
+        for field in self.fields[1:]:
+            value = train[field]
+            if isinstance(value, int) and value <= 5:
+                value = log(value, 'r')
+            train_fields.append(value)
+        self.add_row(
+            train['车次'],
+            train_fields
+        )
+
+    def add_row(self, key: str, value: list):
+        _value = list()
+        for v in value:
+            if isinstance(v, int) and v <= 5:
+                _value.append(log(v, 'r'))
+            else:
+                _value.append(v)
+        _value.insert(0, log(key, color='g'))
+        self.pt.add_row(_value)
+
+    def add_column(self, key: str, value: list):
+        self.pt.add_column(key, value)
+
+    @property
+    def sheet(self):
+        return self.pt
+
+
+if __name__ == '__main__':
+    print(log('asdas', 'b'))
--- query/ticket.py
+++ query/ticket.py
@@ -4,32 +4,32 @@ import loguru
 import requests
 import re
 import json
-from library.util import proxies
+from library.util import proxies, ticket_sheet, log
 from library.constants import URL, CITY_MAP
 
 requests.packages.urllib3.disable_warnings()
 
 
-class Ticket():
+class Ticket(object):
     def __init__(self, start_point, end__point, journey_date, age_type: str = 'ADULT'):
         self.start_point = start_point
         self.end__point = end__point
         self.journey_date = journey_date
         self.age_type = age_type
 
-
     def send_request(self, kwargs):
         try:
             response = requests.get(**kwargs)
             return response.json()
         except Exception as e:
+            import traceback
+            traceback.print_exc()
             loguru.logger.error('查票接口错误！', e)
             return False
 
-
     def request_config(self):
 
-        journey_date, start_point, end__point, age_type= self.get_point()
+        journey_date, start_point, end__point, age_type = self.get_point()
 
         if not start_point and not end__point:
             return False
@@ -47,7 +47,7 @@ class Ticket():
             "Accept-Language": "zh-CN,zh;q=0.9",
             "Cache-Control": "no-cache",
             "Connection": "keep-alive",
-            "Cookie": "JSESSIONID=63F76990BCFA7B7EF1F4B683AF7CA224; route=6f50b51faa11b987e576cdb301e545c4; BIGipServerotn=1356398858.24610.0000;",
+            "Cookie": "_uab_collina=163843510026522868159604; tk=rfqYT7PvTNCZOToP_E6UmY14rTb_SdvROzUAvgy0z1z0; JSESSIONID=1DCAFFFAC2871CEE540D2C5338512DC4; _jc_save_wfdc_flag=dc; BIGipServerpool_passport=216269322.50215.0000; guidesStatus=off; highContrastMode=defaltMode; cursorStatus=off; current_captcha_type=Z; route=9036359bb8a8a461c164a04f8f50b252; BIGipServerotn=821035530.64545.0000; _jc_save_fromStation=%u6DEE%u5357%2CHAH; _jc_save_toStation=%u676D%u5DDE%2CHZH; RAIL_EXPIRATION=1642683801231; RAIL_DEVICEID=cVLcmUrJTKiuHtXIuu8PEdtodRGYEJUqRcu65-ASHHNuSzHXfsips7uuG-uj79plfAusBcYtWiYgLnHebFDL9Nr5yDg3fSTisl4IU7K7j6UmUH_KP6nlfOszT4q2JQQwJhMH_cVZ78MYJRG41DycP4FtdwgYQ6rn; _jc_save_fromDate=2022-01-18; _jc_save_toDate=2022-01-18",
             "Host": "kyfw.12306.cn",
             "If-Modified-Since": "0",
             "Pragma": "no-cache",
@@ -58,7 +58,7 @@ class Ticket():
             "Sec-Fetch-Dest": "empty",
             "Sec-Fetch-Mode": "cors",
             "Sec-Fetch-Site": "same-origin",
-            "User-Agent": f"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/{random.randint(500,600)}.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+            "User-Agent": f"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/{random.randint(500, 600)}.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
             "X-Requested-With": "XMLHttpRequest"
         }
         return {
@@ -70,7 +70,6 @@ class Ticket():
             # 'proxies': proxies
         }
 
-
     def get_point(self):
         journey_date = self.journey_date
         age_type = self.age_type
@@ -78,113 +77,65 @@ class Ticket():
         try:
             start_point = CITY_MAP.CITY_CODE[self.start_point].upper()
         except Exception:
-            loguru.logger.error(f"出发地址错误: { self.start_point}")
+            loguru.logger.error(f"出发地址错误: {self.start_point}")
 
             start_point = False
-
 
         try:
             end__point = CITY_MAP.CITY_CODE[self.end__point].upper()
         except Exception:
-            loguru.logger.error(f"目标地址错误: { self.end__point}")
+            loguru.logger.error(f"目标地址错误: {self.end__point}")
             end__point = False
 
         return journey_date, start_point, end__point, age_type
 
+    def find_stations(self, stations):
+        return CITY_MAP.CODE_CITY.get(stations, stations)
+
+    def format_ticket(self, ticket):
+        if ticket == '' or ticket == '无' or ticket == '有':
+            return ticket
+        return int(ticket)
 
     def parse_(self, resp_json):
         result = resp_json['data']['result']
         data_list = list()
+        sheet = ticket_sheet()
         for tk_ in result:
             tk_info = tk_.split('预订')[1]
-            pr_info = re.match( r'^\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|([YN])\|',tk_info)
+            pr_info = re.match(
+                r'^\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|([YN])\|',
+                tk_info)
             if pr_info:
                 tk_num_ = tk_info.split('|2022')[1]
                 tk_num = tk_num_.split('|')
-                # train_no
-                # loguru.logger.info(f'train _no: {pr_info.group(1)}')
-                # # 车次
-                # loguru.logger.info(f'车     次: {pr_info.group(2)}')
-                # # 车次起点站
-                # loguru.logger.info(f'车次起点站: {CITY_MAP.CODE_CITY[pr_info.group(3)]}')
-                # # 车次终点站
-                # loguru.logger.info(f'车次终点站: {CITY_MAP.CODE_CITY[pr_info.group(4)]}')
-                # # 起点站
-                # loguru.logger.info(f'起   点站: {CITY_MAP.CODE_CITY[pr_info.group(5)]}')
-                # # 终点站
-                # loguru.logger.info(f'终   点站: {CITY_MAP.CODE_CITY[pr_info.group(6)]}')
-                # # 出发时间git
-                # loguru.logger.info(f'出发 时间: {pr_info.group(7)}')
-                # # 到达时间
-                # loguru.logger.info(f'到达 时间: {pr_info.group(8)}')
-                # # 历时
-                # loguru.logger.info(f'历    时: {pr_info.group(9)}')
-                # # 是否当日到达
-                # loguru.logger.info(f'是否当日到达: {pr_info.group(10)}')
-                # # 商务座、特等座
-                # loguru.logger.info(f'商务座、  特等座: {tk_num[19]}')
-                # # 一等座
-                # loguru.logger.info(f'一等         座: {tk_num[18]}')
-                # # 二等座、二等包座
-                # loguru.logger.info(f'二等座、二等包座: {tk_num[17]}')
-                # # 高级软卧
-                # loguru.logger.info(f'高级       软卧: {tk_num[8]}')
-                # # 软卧、一等卧
-                # loguru.logger.info(f'软卧     一等卧: {tk_num[10]}')
-                # # 动卧
-                # loguru.logger.info(f'动          卧: {tk_num[20]}')
-                # # 硬卧、二等卧
-                # loguru.logger.info(f'软卧     二等卧: {tk_num[15]}')
-                # # 软卧
-                # loguru.logger.info(f'软          卧: {tk_num[11]}')
-                # # 硬座
-                # loguru.logger.info(f'硬          座: {tk_num[16]}')
-                # # 无座
-                # loguru.logger.info(f'无          座: {tk_num[13]}')
-                # # from_station_no
-                # loguru.logger.info(f'from_station_no: {tk_num[3]}')
-                # # to_station_no
-                # loguru.logger.info(f'to_station_no: {tk_num[4]}')
-                # # seat_types
-                # loguru.logger.info(f'seat    types: {tk_num[21]}')
-                #
-                # loguru.logger.info(f'----------------------------')
-                data_list.append({
-                    "train_info": {
-                        'train _no': pr_info.group(1),
-                        '车     次': pr_info.group(2),
-                        '车次起点站': pr_info.group(3),
-                        '车次终点站': pr_info.group(4),
-                        '起   点站': pr_info.group(5),
-                        '终   点站': pr_info.group(6),
-                        '出发 时间': pr_info.group(7),
-                        '到达 时间': pr_info.group(8),
-                        '历    时': pr_info.group(9),
-                        '是否当日到达': pr_info.group(10)
-                    },
-                    "ticket_info": {
-                        '商务座、  特等座': tk_num[19],
-                        '一等         座': tk_num[18],
-                        '二等座、二等包座': tk_num[17],
-                        '高级       软卧': tk_num[8],
-                        '软卧     一等卧': tk_num[10],
-                        '动          卧': tk_num[20],
-                        '软卧     二等卧': tk_num[15],
-                        '软          卧': tk_num[11],
-                        '硬          座': tk_num[16],
-                        '无          座': tk_num[13]
-                    },
-                    "price_info": {
-                        'from_station_no': tk_num[3],
-                        'to_station_no': tk_num[4],
-                        'seat    types': tk_num[21],
-                        'train    date': self.journey_date
-                    }
-                })
+                train_info = {
+                    '车次': pr_info[2],
+                    '车起始': self.find_stations(pr_info[3]),
+                    '车终点': self.find_stations(pr_info[4]),
+                    '起始站': self.find_stations(pr_info[5]),
+                    '终点站': self.find_stations(pr_info[6]),
+                    '出发时间': pr_info[7],
+                    '到达时间': pr_info[8],
+                    '历时': pr_info[9],
+                    '当日到达': pr_info.group(10),
+                    '商务座': self.format_ticket(tk_num[19]),
+                    '一等座': self.format_ticket(tk_num[18]),
+                    '二等座': self.format_ticket(tk_num[17]),
+                    '高级软卧': self.format_ticket(tk_num[8]),
+                    '一等卧': self.format_ticket(tk_num[10]),
+                    '动卧': self.format_ticket(tk_num[20]),
+                    '二等卧': self.format_ticket(tk_num[15]),
+                    '软卧': self.format_ticket(tk_num[11]),
+                    '硬座': self.format_ticket(tk_num[16]),
+                    '无座': self.format_ticket(tk_num[13])
+                }
+                data_list.append(train_info)
+                sheet.add_train(train_info)
             else:
                 loguru.logger.error('当前余票json解析错误')
+        print(sheet.sheet)
         return data_list
-
 
     def query(self):
         ci = self.request_config()
@@ -203,8 +154,8 @@ class Ticket():
 
 if __name__ == '__main__':
     start_point = '杭州'
-    end__point = '芜湖'
-    journey_date = '2022-01-14'
+    end__point = '淮南'
+    journey_date = '2022-01-29'
     age_type = 'ADULT'
     tk_r = Ticket(start_point, end__point, journey_date, age_type).query()
     loguru.logger.info(tk_r)
